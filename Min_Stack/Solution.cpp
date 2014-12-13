32ac3fc426b825140139ae739100718acdb7b798
Khitan Wu <wuxiao356@gmail.com>
2014-12-13 14:53:43 -0600

    >_<

---
+++ Min_Stack/Solution.cpp
@@ -0,0 +1,33 @@
+/**
+* Created by Yu(Khitan) Wu on 12/11/2014
+* Copyright (C) 2014 Yu(Khitna) Wu All rights reserved.
+* Email: wuxiao356@gmial.com
+*/
+
+#include <stack>
+
+class MinStack {
+public:
+	void push(int x) {
+		element.push(x);
+		if (minElement.empty()) minElement.push(x);
+		else if (minElement.top() >= x) minElement.push(x);
+	}
+
+	void pop() {
+		if (minElement.top() == element.top()) minElement.pop();
+		element.pop();
+	}
+
+	int top() {
+		return element.top();
+	}
+
+	int getMin() {
+		return minElement.top();
+	}
+
+private:
+	stack<int> element; // regular stack which is used to store the elements
+	stack<int> minElement; //minimun stack which is used to store the minimum element in the past. That it, when the new element being                            //pushed is a new minimum, push it to the minElement too
+};
